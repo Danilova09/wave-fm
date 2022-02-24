1d376fbe78c7dd8d1f67b1154b09cbda5780f816
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-02-25 04:28:38 +0600

    Adding stylization to the main components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { ArtistsComponent } from './pages/home/artists/artists.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AlbumsComponent } from './pages/home/albums/albums.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  {
+    path: '', component: HomeComponent, children: [
+      {path: '', component: ArtistsComponent},
+      {path: 'albums', component: AlbumsComponent},
+    ]
+  },
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { ArtistsComponent } from './pages/home/artists/artists.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AlbumsComponent } from './pages/home/albums/albums.component';
+import { ArtistItemComponent } from './pages/home/artists/artist-item/artist-item.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistsComponent,
+    AlbumsComponent,
+    ArtistItemComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,7 +38,11 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     IconsProviderModule,
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    HttpClientModule,
+    NzCardModule,
+    NzAvatarModule,
+    NzGridModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
